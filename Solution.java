ffecfd48c781b77b3ba532dac8d26bfa2a386700
Guilherme Augustto <guilhermecosta.barros@gmail.com>
2025-05-25 19:31:02 -0300

    feat: optimized solution

--- Solution.java
+++ Solution.java
@@ -1,3 +1,26 @@
-class Solution {
+import java.util.HashMap;
 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+
+        for(int i = 0; i < nums.length; i++) {
+
+            int targetMenosValorAtual = target - nums[i];
+
+            if(map.containsKey(targetMenosValorAtual)) {
+
+                result[0] = map.get(targetMenosValorAtual);
+                result[1] = i;
+
+                return result;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+
+        throw new IllegalArgumentException("Nenhuma combinação de índices encontrada para o target fornecido.");
+    }
 }
